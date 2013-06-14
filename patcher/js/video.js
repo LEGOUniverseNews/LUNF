e060a1231617d998be22c3b1d5954f9b8fd801fd
Triangle717 <le717@hotmail.com>
2013-06-14 16:37:02 -0300

    Update video.js
    Seeing if a YT video can be embedded

--- patcher/js/video.js
+++ patcher/js/video.js
@@ -12,7 +12,8 @@ flashembed("example",
 		controlBarBackgroundColor:'0x000000',
 		initialScale: 'scale',
 		// LEGO Animation goes here (try to get the best quality and use various themes)
-		videoFile: 'http://cache.lego.com/r/city/-/media/Franchises/LEGO%20City/Movies/Designer%20Videos/MP4/ts.20121205T140622.Joe%20-%20Fire%20Truck.mp4'
+		//videoFile: 'http://cache.lego.com/r/city/-/media/Franchises/LEGO%20City/Movies/Designer%20Videos/MP4/ts.20121205T140622.Joe%20-%20Fire%20Truck.mp4'
+		videoFile: 'http://www.youtube.com/embed/z-St6ogOIzw?rel=0'
                         
 	}} 
 );
